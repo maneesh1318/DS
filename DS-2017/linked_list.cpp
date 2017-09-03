d6cc9257bc94886613f95c8c2bf3ec5619053108
Manish Richhariya <maneesh1318@gmail.com>
2017-09-03 19:04:54 +0530

    First Commit

---
+++ DS-2017/linked_list.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+class LinkedList{
+	string d;
+	LinkedList *p;
+	public:
+		LinkedList(){
+			p = null;
+		}
+		
+		void print(){
+			if(p==null) return;
+			while(p!=null){
+				cout<<p
+			}
+		}
+}
+
+int main(){
+	
+}
